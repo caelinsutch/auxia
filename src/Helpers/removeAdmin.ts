a3570409aee2c0caecf504e1334dd9a773925365
Ethan Wainer <ethan@wainermail.com>
2022-03-18 11:43:45 -1000

    resolved error in removeAdmin

--- src/Helpers/removeAdmin.ts
+++ src/Helpers/removeAdmin.ts
@@ -3,13 +3,16 @@ import {Response} from "../Types/Response";
 import {organizationCollection} from "../Firebase";
 import {arrayRemove, updateDoc} from "firebase/firestore";
 
-const removeAdmin = async (organization: Organization, adminPhoneNumber: string): Promise<Response> => {
-    organization.adminPhoneNumbers.push(adminPhoneNumber);
+const removeAdmin = async (organization: Organization, adminPhoneNumber: string, adminName: string): Promise<Response> => {
+    // todo change Admins from an array of numbers to a mapping of numbers to names
+    // todo untested
+    organization.adminPhoneNumbers[adminPhoneNumber].name = adminName;
 
     const adminDoc = organizationCollection.doc(organization.organizationId);
 
     // todo will this work
     // @ts-ignore
+    // todo update this query to update the admins with the new map structure
     const res = await updateDoc(adminDoc, { // todo get return value or check for error
         admins: arrayRemove(adminPhoneNumber)
     });
