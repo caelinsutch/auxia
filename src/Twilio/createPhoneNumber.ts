d4c1dfcf76c8331e9823c4d32d78676995dde6d7
Ethan Wainer <ethan@wainermail.com>
2022-03-26 01:43:46 -0700

    logs for incoming

--- src/Twilio/createPhoneNumber.ts
+++ src/Twilio/createPhoneNumber.ts
@@ -1,28 +1,34 @@
 import twilioClient from './twilioClient';
 
 const createPhoneNumber = async (chosenNumber: string = "", lineWith: string, isOrganization: boolean) => {
-    /* todo scheme for getting/creating new SMS URLs?
-    *  todo create function to manage creating a new webhook(?) for each organization, or just use one hook?
-    *   probably safer to have a unique webhook URL for each organization, wouldn't have to DB lookup for each org
-    */
 
-    const orgSmsUrl = "https://rqx5xs6mx9.execute-api.us-east-1.amazonaws.com/dev/incoming"; // todo change
+    let smsUrl;
+    let friendlyName;
     let opts = {}
-    const friendlyName = isOrganization ? `MAIN LINE number for ${ lineWith }` : `TEXT LINE with ${ lineWith }`;
+
+    if (isOrganization) {
+        friendlyName = `MAIN LINE number for ${ lineWith }`;
+        smsUrl = "https://auxia-production.up.railway.app/api/handleIncoming"
+    } else {
+        friendlyName = `TEXT LINE with ${ lineWith }`;
+        smsUrl = "https://auxia-production.up.railway.app/api/handleExternal";
+    }
 
     if (chosenNumber) {
         opts = {
             phoneNumber: chosenNumber,
-            // smsUrl: orgSmsUrl,
-            friendlyName: friendlyName
+            smsUrl: smsUrl,
+            friendlyName: friendlyName,
+            smsMethod: "POST"
         }
     } else {
         // todo if they haven't chosen a number get a new one instead of area code registration
         const num = await twilioClient.availablePhoneNumbers('US').fetch();
         opts = {
             areaCode: "510",
-            // smsUrl: orgSmsUrl,
-            friendlyName: friendlyName
+            smsUrl: smsUrl,
+            friendlyName: friendlyName,
+            smsMethod: "POST"
         }
     }
 
