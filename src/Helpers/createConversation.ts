3a76f5b56091d0b275cbef140ef088116157ff7e
Ethan Wainer <ethan@wainermail.com>
2022-03-29 16:52:38 -0700

    added CL insertion

--- src/Helpers/createConversation.ts
+++ src/Helpers/createConversation.ts
@@ -1,10 +1,15 @@
-import {organizationCollection} from '../Firebase';
+import {conversationLineCollection, organizationCollection} from '../Firebase';
 import {createPhoneNumber} from '../Twilio';
 import {Conversation} from '../Types/Organization';
 
 const createConversation = async (organizationId: string, phoneNumber: string): Promise<Conversation> => {
     const organizationDoc = organizationCollection.doc(organizationId);
     const newPhoneNumber = await createPhoneNumber("", phoneNumber, false);
+
+    const lineUpdate = await conversationLineCollection.doc(newPhoneNumber).set({
+        organizationId: organizationId,
+        userNumber: phoneNumber
+    }); // todo return check for status
 
     const conversation: Conversation = {
         conversationLine: newPhoneNumber
