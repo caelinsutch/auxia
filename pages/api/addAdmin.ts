af554f384ccbbe27f4160b81dbb6abc54732be3a
Ethan Wainer <ethan@wainermail.com>
2022-03-31 15:25:48 -0700

    added routes for adding admins and organization

---
+++ pages/api/addAdmin.ts
@@ -0,0 +1,35 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type {NextApiRequest, NextApiResponse} from 'next'
+import addAdmin from "../../src/Helpers/addAdmin";
+import getOrganization from "../../src/Helpers/getOrganization";
+
+type Data = {
+    name: string
+    data: any
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    let response = {
+        status: 1,
+        data: "testing"
+    }
+
+    const organizationId = req.body.organizationId;
+    const adminPhoneNumber = req.body.adminPhoneNumber;
+    const adminName = req.body.adminName;
+
+    console.log(organizationId, adminPhoneNumber, adminName)
+
+    if (!organizationId || !adminPhoneNumber || !adminName) {
+        return res.json({name: "response", data: {status: 0, data: "error malformed request"}})
+    }
+    const organization = await getOrganization(organizationId);
+    const result = await addAdmin(organization, adminPhoneNumber, adminName);
+    // todo add errors
+    result.data = {status: 1, response: result}
+
+    res.json({name: "response", ...response})
+}
---
+++ pages/api/createOrganizationApi.ts
@@ -0,0 +1,34 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type {NextApiRequest, NextApiResponse} from 'next'
+import createOrganization from "../../src/Helpers/createOrganization";
+
+type Data = {
+    name: string
+    data: any
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    let response = {
+        status: 1,
+        data: "testing"
+    }
+
+    const organizationName = req.body.organizationName;
+    const organizationUniversity = req.body.organizationUniversity;
+    const ownerPhoneNumber = req.body.ownerPhoneNumber;
+    const chosenPhoneNumber = req.body.chosenPhoneNumber || "";
+
+    if (!organizationName || !organizationUniversity || !ownerPhoneNumber) {
+        return res.json({name: "response", data: {status: 0, data: "error malformed request"}})
+    }
+
+    const newOrganization = await createOrganization(organizationName, organizationUniversity, ownerPhoneNumber, chosenPhoneNumber);
+    if (typeof newOrganization === "object") { // if it has returned the new Organization object
+        response.data = "success";
+    }
+
+    res.json({name: "response", ...response})
+}
